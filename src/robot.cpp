25b65f76a4e0d5320aaae346ac046f6d6521f441
JamesJMakhlouf <jamesjmakhlouf@gmail.com>
2026-07-05 00:39:00 +0300

    feat: add Robot base class configuration and implementation

---
+++ src/robot.cpp
@@ -0,0 +1,66 @@
+#include "robot.hpp"
+#include <iostream>
+#include <algorithm>
+
+Robot::Robot(const std::string& id, const std::string& name, int battery) : id_(id), name_(name), status_("idle") {
+    battery_ = std::max(0, std::min(100, battery));
+}
+
+void Robot::work() {
+    if(battery_ < 5) {
+        std::cout << "Low battery, please charge.\n";
+        return;
+    }
+    battery_ -=5;
+    status_ = "working";
+}
+
+void Robot::charge() {
+    if (battery_ == 100) {
+        std::cout << "Battery full.\n";
+        return;
+    }
+    battery_ = 100;
+    status_ = "charging";
+}
+
+std::string Robot::id() const {
+    return id_;
+}
+    
+std::string Robot::name() const {
+    return name_;
+}
+
+int Robot::battery() const {
+    return battery_;
+}
+
+std::string Robot::status() const {
+    return status_;
+}
+
+bool Robot::operator==(const Robot& other) const {
+    return id_ == other.id();
+}
+
+std::string Robot::operator+(const Robot& other) const {
+    return name_ + " + " + other.name();
+}
+
+// Why friend and not a regular method?
+// In `std::coud << robot`, the left-hand operand is an `srd::ostream`,
+// not a Robot object. C++ member function require the host class
+// object to be on the left side, so this must be a non-member free
+// function
+// Because it is a free function standing outside the class, it would
+// normally be blocked from accessing proteeected or private members.
+// Marking it as `friend` inside the header grants it explicit permission
+// to look inside.
+std::ostream& operator<<(std::ostream& os, const Robot& r) {
+    os << "[" << r.type() << "] ID: " << r.id() 
+       << ", Name: " << r.name() 
+       << ", Battery: " << r.battery() << "%"
+       << ", Status: " << r.status();
+    return os;
+}
