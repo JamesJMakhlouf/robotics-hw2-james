22529dd44b01b0e8cfdf01f96c2d96c43ea67d3e
JamesJMakhlouf <jamesjmakhlouf@gmail.com>
2026-07-05 12:36:24 +0300

    feat: add Task struct

---
+++ src/task.cpp
@@ -0,0 +1,10 @@
+#include "task.hpp"
+
+bool Task::operator<(const Task& other) const {
+    return priority < other.priority;
+}
+
+std::ostream& operator<<(std::ostream& os, const Task& t) {
+    os << "Task: \"" << t.name << "\" [Priority: " << t.priority
+       << ", Assigned to: " << t.assigned_to << "]";
+}
