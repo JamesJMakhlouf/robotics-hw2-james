9358ea371c635e422adf7e98e7349654944389a7
JamesJMakhlouf <jamesjmakhlouf@gmail.com>
2026-07-05 17:35:00 +0300

    fix: upgrade robots container to unordered_map for O(1) lookups

--- include/fleet.hpp
+++ include/fleet.hpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <string>
 #include <ostream>
-#include <map>
+#include <unordered_map>
 #include <queue>
 
 class Fleet {
@@ -30,14 +30,14 @@ public:
     void find_low_battery() const;
 
 private:
-    // std::map allows the fleet to find or remoce any robot by its
-    // unique id without performing slow linear scans. 
+    // std::unordered_map allows the fleet to find or remove any robot by its
+    // unique id in constant time, without performing slow linear scans. 
 
     // we use std::shared_ptr instead of std::unique_ptr because multiple
     // parts of the program need access to the same robot at the same time.
     // for example, both the Fleet and active background threads need to reference
     // a robot without stripping ownership from one another.
-    std::map<std::string, std::shared_ptr<Robot>> robots_;
+    std::unordered_map<std::string, std::shared_ptr<Robot>> robots_;
     
     // std::priority_queue maintains elements sorted according to their
     // priority, guarateeing that the the highest priority tasks are
